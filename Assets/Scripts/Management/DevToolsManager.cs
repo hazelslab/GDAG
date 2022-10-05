e5946e175f1298e5f35defe2c84e38507f75f792
pulpxi <hadi.hassan.2000@outlook.com>
2022-10-05 15:09:52 +0200

    DevToolsPanel added

---
+++ Assets/Scripts/Management/DevToolsManager.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DevToolsManager : MonoBehaviour
+{
+    
+    public bool DevToolsOpened { get; private set; }
+
+    [SerializeField]
+    private GameObject _DevToolsPanel;
+
+    private void Awake()
+    {
+        if (_DevToolsPanel.activeSelf)
+        {
+            DevToolsOpened = true;
+        }
+        else
+        {
+            DevToolsOpened = false;
+        }
+    }
+
+    public void ToggleDevToolsPanel()
+    {
+        if (DevToolsOpened)
+        {
+            _DevToolsPanel.SetActive(false);
+        }
+        else
+        {
+            _DevToolsPanel.SetActive(true);
+        }
+        DevToolsOpened = !DevToolsOpened;
+    }
+}
